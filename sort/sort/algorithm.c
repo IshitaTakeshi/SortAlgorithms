d5f0fa0006a36dbe07093a8595631660023add82
sonicair <ishitah.takeshi@gmail.com>
2015-12-31 01:50:30 +0900

    Remove sort/sort

--- sort/sort/algorithm.c
+++
@@ -1,8 +0,0 @@
-#include "algorithm.h"
-
-void swap(int *a, int *b) {
-    int tmp;
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
--- sort/sort/basicio.h
+++
@@ -1,5 +0,0 @@
-int get_num_lines(char filename[]);
-void read_data_from_file(char filename[], int array[], int array_size);
-void write_data_to_file(char filename[], int array[], int array_size);
-void read_n_digits(int array[], int N);
-void printarray(int array[], int N);
